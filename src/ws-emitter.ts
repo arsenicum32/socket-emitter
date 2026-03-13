5ffe7b90483d98a08a87da4e2fccb3c7e250ef81
Arseniy <zombiehot@yandex.ru>
2026-03-13 18:14:00 +0300

    feat: add configurable reconnect policy

--- src/ws-emitter.ts
+++ src/ws-emitter.ts
@@ -7,15 +7,20 @@ const ERROR_EVENT = 'error'
 
 export interface OptionsType {
   autoConnect?: boolean
+  autoReconnect?: boolean
+  reconnectTimeout?: number
 }
 
 const defaultOptions: Required<OptionsType> = {
   autoConnect: true,
+  autoReconnect: true,
+  reconnectTimeout: 1000,
 }
 
 export class WsEmitter extends EventEmitter {
   private readonly url: string
   private readonly options: Required<OptionsType>
+  private reconnectTimeout = 0
 
   socket: WebSocket | undefined
 
@@ -23,6 +28,21 @@ export class WsEmitter extends EventEmitter {
     super()
     this.url = url
     this.options = { ...defaultOptions, ...options }
+
+    this.on(CLOSE_EVENT, () => {
+      if (this.options.autoReconnect) {
+        setTimeout(this.connect.bind(this), this.reconnectTimeout)
+        this.reconnectTimeout = this.options.reconnectTimeout
+      }
+    })
+
+    this.on(OPEN_EVENT, () => {
+      this.reconnectTimeout = 0
+    })
+
+    this.on(ERROR_EVENT, () => {
+      this.socket?.close()
+    })
 
     if (this.options.autoConnect) {
       this.connect()
