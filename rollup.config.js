ea62e064dc23608fb76da72f03c00b1737b112cd
Arseniy <zombiehot@yandex.ru>
2026-03-22 11:20:00 +0300

    chore: add rollup bundles for cjs and esm consumers

---
+++ rollup.config.js
@@ -0,0 +1,24 @@
+import { nodeResolve } from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import { terser } from 'rollup-plugin-terser'
+
+export default {
+  input: 'dist/index.js',
+  output: [
+    {
+      file: 'dist/bundle.cjs.js',
+      format: 'cjs',
+      sourcemap: true,
+    },
+    {
+      file: 'dist/bundle.esm.js',
+      format: 'esm',
+      sourcemap: true,
+    },
+  ],
+  plugins: [
+    nodeResolve(),
+    commonjs(),
+    terser(),
+  ],
+}
